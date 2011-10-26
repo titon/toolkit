ff84910bb5c1cd262cf8b81cfba6bf5783082cf7
Miles Johnson <mileswjohnson@gmail.com>
2011-10-25 23:46:51 -0700

    Attribute config parsing.

--- src/Titon.js
+++ src/Titon.js
@@ -11,6 +11,42 @@ var Titon = {
 	
 	setup: function(config) {
 		Titon.config = Object.merge(Titon.config, config);
+	},
+	
+	convertType: function(value) {
+		if (value == 'true') {
+			value = true;
+			
+		} else if (value == 'false') {
+			value = false;
+			
+		} else if (value == 'null') {
+			value = null;
+			
+		} else if (isNaN(value)) {
+			value = String.from(value);
+			
+		} else {
+			value = Number.from(value);
+		}
+		
+		return value;
+	},
+	
+	parseConfig: function(data) {
+		var config = {};
+		
+		if (data) {
+			data.split(';').each(function(item) {
+				var pieces = item.split(':');
+
+				if (pieces.length) {
+					config[pieces[0]] = Titon.convertType(pieces[1]);
+				}
+			});
+		}
+		
+		return config;
 	}
 	
 };
