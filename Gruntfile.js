7177d8cac721b8b0fe9c84019f3fe6c9d25189e3
Miles Johnson <mileswjohnson@gmail.com>
2013-04-12 17:17:52 -0700

    Added concat, uglify and compress to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,33 +2,6 @@ module.exports = function(grunt) {
 	var _ = grunt.util._, // http://lodash.com/docs
 		log = grunt.log;
 
-	// Re-usable options
-	var options = {
-		jshint: {
-			browser: true,
-			mootools: true,
-			globals: {
-				Titon: true,
-				Timers: true
-			},
-			// enforcing
-			camelcase: true,
-			curly: true,
-			eqeqeq: true,
-			immed: true,
-			latedef: true,
-			noempty: true,
-			quotmark: 'single',
-			smarttabs: true,
-			undef: true,
-			unused: true,
-			strict: true,
-			trailing: true,
-			// relaxing
-			boss: true
-		}
-	};
-
 	// Component tree and dependencies
 	var manifest = {
 		base: {
@@ -106,6 +79,9 @@ module.exports = function(grunt) {
 	var toPackage = grunt.option('package') ? grunt.option('package').split(',') : _.keys(manifest),
 		dependencies = {};
 
+	toPackage.forEach(addDependency);
+
+	// Helper functions
 	function addDependency(name) {
 		if (!manifest[name]) {
 			log.error('Invalid component: ' + name);
@@ -127,35 +103,133 @@ module.exports = function(grunt) {
 				}));
 			}
 		});
+
+		toPackage = _.union([name], toPackage);
 	}
 
-	toPackage.forEach(addDependency);
+	function mapSass(css) {
+		var map = {};
+
+		css.forEach(function(path) {
+			map[path] = path.replace(/css/g, 'scss');
+		});
+
+		return map;
+	}
+
+	function createBanner() {
+		return "/*!\n" +
+			" * Titon Toolkit v<%= pkg.version %>\n" +
+			" * <%= pkg.copyright %> - <%= pkg.homepage %>\n" +
+			" * <%= pkg.licenses[0].type %> - <%= pkg.licenses[0].url %>\n" +
+			" * Components: " + toPackage.join(', ') + "\n" +
+			" */\n";
+	}
 
 	// Configure
 	grunt.initConfig({
 		// Package schema - https://npmjs.org/doc/json.html
 		pkg: grunt.file.readJSON('package.json'),
+		buildFile: 'build/<%= pkg.name.toLowerCase() %>-<%= pkg.version %>',
 
-		// JShint - http://jshint.com/docs/
+		// 1) Validate the Javascript source directory
+		// http://jshint.com/docs/
 		jshint: {
-			src: {
-				src: ['src/js/**/*.js'],
-				options: options.jshint
+			options: {
+				globals: {
+					Titon: true,
+					Timers: true
+				},
+				browser: true,
+				mootools: true,
+				// enforcing
+				camelcase: true,
+				curly: true,
+				eqeqeq: true,
+				immed: true,
+				latedef: true,
+				noempty: true,
+				quotmark: 'single',
+				smarttabs: true,
+				undef: true,
+				unused: true,
+				strict: true,
+				trailing: true,
+				// relaxing
+				boss: true
+			},
+			build: {
+				src: ['src/js/**/*.js']
+			}
+		},
+
+		// 2) Generate new CSS files before building
+		// https://github.com/gruntjs/grunt-contrib-sass
+		sass: {
+			options: {
+				style: 'compressed',
+				compass: 'src/config.rb'
+			},
+			build: {
+				files: mapSass(dependencies.css)
+			}
+		},
+
+		// 3) Combine the JS and CSS components into a single file
+		// https://npmjs.org/package/grunt-contrib-concat
+		concat: {
+			options: {
+				banner: createBanner(),
+				separator: ""
+			},
+			build: {
+				files: [
+					{ src: dependencies.css, dest: '<%= buildFile %>.min.css' },
+					{ src: dependencies.js, dest: '<%= buildFile %>.min.js' }
+				]
+			}
+		},
+
+		// 4) Minify Javascript (CSS was minified in step 2)
+		// http://lisperator.net/uglifyjs/
+		uglify: {
+			options: {
+				report: 'min',
+				banner: createBanner()
+			},
+			build: {
+				files: {
+					'<%= buildFile %>.min.js': '<%= buildFile %>.min.js'
+				}
+			}
+		},
+
+		// 5) Archive the files and docs into a zip
+		// https://npmjs.org/package/grunt-contrib-compress
+		compress: {
+			options: {
+				mode: 'zip',
+				pretty: true,
+				archive: '<%= buildFile %>.zip'
+			},
+			build: {
+				files: [
+					{ src: '*.css', dest: 'css/', cwd: 'build/', expand: true },
+					{ src: '*.js', dest: 'js/', cwd: 'build/', expand: true },
+					{ src: '*.md' }
+				]
 			}
 		}
-
-		// CSSLint - https://github.com/stubbornella/csslint/wiki/Rules
-		// Conflicts too much with SASS
 	});
 
 	// Load plugins
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-csslint');
-	//grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-compress');
+	grunt.loadNpmTasks('grunt-contrib-sass');
 
 	// Register tasks
 	grunt.registerTask('validate', ['jshint']);
-	grunt.registerTask('default', ['jshint']);//, ['concat', 'uglify']);
+	grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'compress']);
 };
--- src/config.rb
+++ src/config.rb
@@ -1,7 +1,6 @@
 # Require any additional compass plugins here.
 # http://compass-style.org/reference/compass/
 
-# Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css/"
 sass_dir = "scss/"
@@ -9,18 +8,5 @@ images_dir = "img/"
 javascripts_dir = "js/"
 
 preferred_syntax = :scss
-
-# You can select your preferred output style here (can be overridden via the command line):
-#output_style = :compressed
-
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
-
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass css scss && rm -rf sass && mv scss sass
+output_style = :compressed
+line_comments = false
