b73fda9e6cee964e96d2475672e53670eac47272
Miles Johnson <mileswjohnson@gmail.com>
2013-04-15 20:20:52 -0700

    Added upcoming modules

---
+++ src/js/modules/TypeAhead.js
@@ -0,0 +1,14 @@
+/**
+ * @copyright	Copyright 2010-2013, The Titon Project
+ * @license		http://opensource.org/licenses/bsd-license.php
+ * @link		http://titon.io
+ */
+
+(function() {
+	'use strict';
+
+Titon.TypeAhead = new Class({
+	Extends: Titon.Module
+});
+
+})();
