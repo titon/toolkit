4987b3d97f52ad26bc167eb144691427a7f3949d
Miles Johnson <mileswjohnson@gmail.com>
2013-08-09 16:42:19 -0700

    Updated labels and badges to scale

--- examples/label-badge.php
+++ examples/label-badge.php
@@ -1,17 +1,39 @@
 <div class="example-header">Badge</div>
 
 <div class="example">
-	<?php foreach ($types as $type => $typeTitle) { ?>
-		<span class="badge <?php echo $type; ?>"><?php echo rand(0, 500); ?></span>
-	<?php } ?>
+	<h1>Heading 1 <span class="badge"><?php echo rand(0, 500); ?></span></h1>
+	<h2>Heading 2 <span class="badge info"><?php echo rand(0, 500); ?></span></h2>
+	<h3>Heading 3 <span class="badge warning"><?php echo rand(0, 500); ?></span></h3>
+	<h4>Heading 4 <span class="badge success"><?php echo rand(0, 500); ?></span></h4>
+	<h5>Heading 5 <span class="badge error"><?php echo rand(0, 500); ?></span></h5>
+	<h6>Heading 6 <span class="badge"><?php echo rand(0, 500); ?></span></h6>
+
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+		<?php foreach ($types as $type => $typeTitle) { ?>
+			<span class="badge <?php echo $type; ?>"><?php echo rand(0, 500); ?></span>
+		<?php } ?>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+	</p>
 </div>
 
 <div class="example-header">Label</div>
 
 <div class="example">
-	<?php foreach ($types as $type => $typeTitle) { ?>
-		<span class="label <?php echo $type; ?>"><?php echo $typeTitle; ?></span>
-	<?php } ?>
+	<h1>Heading 1 <span class="label label--left">Default</span></h1>
+	<h2>Heading 2 <span class="label label--left info">Info</span></h2>
+	<h3>Heading 3 <span class="label label--left warning">Warning</span></h3>
+	<h4>Heading 4 <span class="label label--left success">Success</span></h4>
+	<h5>Heading 5 <span class="label label--left error">Error</span></h5>
+	<h6>Heading 6 <span class="label label--right">New</span></h6>
+
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+		<?php foreach ($types as $type => $typeTitle) { ?>
+			<span class="label <?php echo $type; ?>"><?php echo $typeTitle; ?></span>
+		<?php } ?>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+	</p>
 </div>
 
 <div class="example">
