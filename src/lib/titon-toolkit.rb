e942c4044852fb51d15ccfd2306a4f7ae7c1c602
Miles Johnson <mileswjohnson@gmail.com>
2013-09-11 21:52:15 -0700

    Update compass config

--- src/lib/titon-toolkit.rb
+++ src/lib/titon-toolkit.rb
@@ -2,10 +2,7 @@ require 'compass'
 
 root = File.join(File.dirname(__FILE__), "..")
 
-if defined?(Compass)
-	Compass::Frameworks.register("toolkit",
-		:path => root,
-		:stylesheets_directory => File.join(root, "scss"),
-		:templates_directory => File.join(root, "templates")
-	)
-end
+Compass::Frameworks.register("toolkit",
+	:stylesheets_directory => File.join(root, "scss"),
+	:templates_directory => File.join(root, "templates")
+)
