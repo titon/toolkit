1189f65ca6be851b5e971c1f40153d75596c9047
Miles Johnson <miles.johnson@hyfn.com>
2013-09-16 17:16:01 -0700

    Updated gem files

--- lib/titon-toolkit.rb
+++ lib/titon-toolkit.rb
@@ -3,6 +3,9 @@ require 'compass'
 root = File.join(File.dirname(__FILE__), "..")
 
 Compass::Frameworks.register("toolkit",
-    :stylesheets_directory => File.join(root, "scss"),
-    :templates_directory => File.join(root, "templates")
+    :path => root,
+    :stylesheets_directory => File.join(root, "scss")
 )
+
+module TitonToolkit
+end
