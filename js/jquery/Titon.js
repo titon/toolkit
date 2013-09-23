bbdb8101f27dc5c31c4010031f237c6a78565cfc
Miles Johnson <mileswjohnson@gmail.com>
2013-09-22 20:20:15 -0700

    Linting

--- js/jquery/Titon.js
+++ js/jquery/Titon.js
@@ -248,28 +248,27 @@ if (!Function.prototype.create) {
  */
 if (!Function.prototype.bind) {
     Function.prototype.bind = function(that) {
-		var self = this,
-			args = arguments.length > 1 ? [].slice.call(arguments, 1) : null,
-			F = function(){};
+        var self = this,
+            args = arguments.length > 1 ? [].slice.call(arguments, 1) : null,
+            F = function(){};
 
-		var bound = function() {
-			var context = that,
+        var bound = function() {
+            var context = that,
                 length = arguments.length;
 
-			if (this instanceof bound){
-				F.prototype = self.prototype;
-				context = new F;
-			}
+            if (this instanceof bound){
+                F.prototype = self.prototype;
+                context = new F();
+            }
 
-			var result = (!args && !length)
-				? self.call(context)
-				: self.apply(context, args && length ? args.concat([].slice.call(arguments)) : args || arguments);
+            var result = (!args && !length) ? self.call(context) :
+                self.apply(context, args && length ? args.concat([].slice.call(arguments)) : args || arguments);
 
-			return context == that ? result : context;
-		};
+            return (context === that) ? result : context;
+        };
 
-		return bound;
-	};
+        return bound;
+    };
 }
 
 })(window);
