914a886ffaa101745474a548cea53482c27cfa15
Miles Johnson <miles.johnson@hyfn.com>
2013-10-24 11:28:17 -0700

    Refactored breadcrumbs to use a divider

--- examples/breadcrumbs.php
+++ examples/breadcrumbs.php
@@ -3,31 +3,31 @@
 <div class="example">
     <nav class="breadcrumbs large">
         <ol>
-            <li><a href="">Home <span class="caret-right"></span></a></li>
-            <li><a href="">Sub-page <span class="caret-right"></span></a></li>
-            <li><a href="">Another page <span class="caret-right"></span></a></li>
-            <li><a href="">Even more <span class="caret-right"></span></a></li>
-            <li><a href="">Current page <span class="caret-right"></span></a></li>
+            <li><a href="">Home <span class="divider">/<span></a></li>
+            <li><a href="">Sub-page <span class="divider">/<span></a></li>
+            <li><a href="">Another page <span class="divider">/<span></a></li>
+            <li><a href="">Even more <span class="divider">/<span></a></li>
+            <li><a href="">Current page <span class="divider">/<span></a></li>
         </ol>
     </nav>
 
     <nav class="breadcrumbs">
         <ol>
-            <li><a href="">Home <span class="caret-right"></span></a></li>
-            <li><a href="">Sub-page <span class="caret-right"></span></a></li>
-            <li><a href="">Another page <span class="caret-right"></span></a></li>
-            <li><a href="">Even more <span class="caret-right"></span></a></li>
-            <li><a href="">Current page <span class="caret-right"></span></a></li>
+            <li><a href="">Home <span class="divider">/<span></a></li>
+            <li><a href="">Sub-page <span class="divider">/<span></a></li>
+            <li><a href="">Another page <span class="divider">/<span></a></li>
+            <li><a href="">Even more <span class="divider">/<span></a></li>
+            <li><a href="">Current page <span class="divider">/<span></a></li>
         </ol>
     </nav>
 
     <nav class="breadcrumbs small">
         <ol>
-            <li><a href="">Home <span class="caret-right"></span></a></li>
-            <li><a href="">Sub-page <span class="caret-right"></span></a></li>
-            <li><a href="">Another page <span class="caret-right"></span></a></li>
-            <li><a href="">Even more <span class="caret-right"></span></a></li>
-            <li><a href="">Current page <span class="caret-right"></span></a></li>
+            <li><a href="">Home <span class="divider">/<span></a></li>
+            <li><a href="">Sub-page <span class="divider">/<span></a></li>
+            <li><a href="">Another page <span class="divider">/<span></a></li>
+            <li><a href="">Even more <span class="divider">/<span></a></li>
+            <li><a href="">Current page <span class="divider">/<span></a></li>
         </ol>
     </nav>
 </div>
