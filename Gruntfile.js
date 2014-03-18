e4fbce7636aa84b1802018d4d4278b5cbb9970e4
Miles Johnson <miles.johnson@hyfn.com>
2014-03-18 12:40:48 -0700

    Greatly reduce Grunt complexity. Removed individual dist files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,8 @@ var compartment = require('compartment');
 
 module.exports = function(grunt) {
     var _ = grunt.util._,
-        graph = new compartment();
+        graph = new compartment(),
+        banner = "/*! Titon Toolkit v<%= pkg.version %> | <%= pkg.licenses[0].type %> License | <%= pkg.homepage %> */\n";
 
     graph.loadManifest(__dirname + '/manifest.json');
     graph.addTypes({
@@ -26,6 +27,7 @@ module.exports = function(grunt) {
         useTheme = grunt.option('theme') || null,
         categories = ['layout', 'component'];
 
+    // If --demo is passed, enable all components and effects
     if (grunt.option('demo')) {
         toPackage = [];
         useEffects = [];
@@ -33,6 +35,7 @@ module.exports = function(grunt) {
         categories.push('effect');
     }
 
+    // If no components are defined, include all from the defined categories
     if (!toPackage.length) {
         _.each(graph.manifest, function(value, key) {
             if (_.contains(categories, value.category)) {
@@ -41,11 +44,13 @@ module.exports = function(grunt) {
         });
     }
 
+    // If --theme is passed, include the theme in the output
     if (useTheme) {
         categories.push('theme');
         toPackage.push('theme-' + useTheme);
     }
 
+    // If --effects is passed, include the effects files in the package
     if (useEffects.length) {
         categories.push('effect');
         toPackage.push(useEffects.map(function(value) {
@@ -53,9 +58,7 @@ module.exports = function(grunt) {
         }));
     }
 
-    /**
-     * Include or remove normalize from the output.
-     */
+    // If --no-normalize is passed, include or remove normalize from the output
     var hasNormalize = _.contains(toPackage, 'normalize');
 
     if (!grunt.option('no-normalize')) {
@@ -72,88 +75,21 @@ module.exports = function(grunt) {
     graph.buildChain(toPackage, categories);
 
     var jsPaths = graph.getPaths('js'),
-        mooUglifyPaths = {},
-        mooConcatPaths = [],
-        jqUglifyPaths = {},
-        jqConcatPaths = [],
-        cssPaths = graph.getPaths('css');
-
-    jsPaths.forEach(function(path) {
-        var mooPath = path.replace('library', 'mootools'),
-            jqPath = path.replace('library', 'jquery');
-
-        mooConcatPaths.push(mooPath);
-        mooUglifyPaths['build/' + mooPath] = mooPath;
-
-        // jQuery doesn't have these files
-        if (!_.contains(path, 'Cache.js') && !_.contains(path, 'Timers.js')) {
-            jqConcatPaths.push(jqPath);
-            jqUglifyPaths['build/' + jqPath] = jqPath;
-        }
-    });
-
-    /**
-     * Generate the banner to place at the top of each file.
-     */
-    function createBanner(hideDeps) {
-        var comps = _.keys(graph.chain).join(','),
-            banner = "/*!\n" +
-                " * Titon Toolkit v<%= pkg.version %>\n" +
-                " * <%= pkg.copyright %> - <%= pkg.homepage %>\n" +
-                " * <%= pkg.licenses[0].type %> - <%= pkg.licenses[0].url %>\n";
-
-        if (comps && !hideDeps) {
-            banner += " * Components: " + comps + "\n";
-        }
-
-        banner += " */\n";
-
-        return banner;
-    }
-
-    /**
-     * Generate a list of file paths for the dist folder.
-     */
-    function prepareDistribution(css, jquery, mootools) {
-        var list = {
-            'dist/toolkit.min.css': css,
-            'dist/toolkit-jquery.min.js': jquery,
-            'dist/toolkit-mootools.min.js': mootools,
-            'dist/jquery/toolkit.min.js': [jquery[0], jquery[1]],
-            'dist/mootools/toolkit.min.js': [mootools[0], mootools[1]]
-        }, name;
-
-        jquery.forEach(function(path, index) {
-            if (index <= 1) {
-                return;
-            }
-
-            name = path.replace('build/js/jquery/components/', '').replace('.js', '.min.js').toLowerCase();
-            list['dist/jquery/toolkit-' + name] = path;
+        cssPaths = graph.getPaths('css'),
+        mooPaths = jsPaths.map(function(path) {
+            return path.replace('library', 'mootools');
+        }),
+        jqueryPaths = jsPaths.map(function(path) {
+            return path.replace('library', 'jquery');
+        }).filter(function(path) {
+            return !_.contains(path, 'Cache.js') && !_.contains(path, 'Timers.js');
         });
-
-        mootools.forEach(function(path, index) {
-            if (index <= 1) {
-                return;
-            }
-
-            if (_.contains(path, 'class')) {
-                name = path.replace('build/js/mootools/class/', 'class.').replace('.js', '.min.js').toLowerCase();
-            } else {
-                name = path.replace('build/js/mootools/components/', '').replace('.js', '.min.js').toLowerCase();
-            }
-
-            list['dist/mootools/toolkit-' + name] = path;
-        });
-
-        return list;
-    }
 
     // Configure
     grunt.initConfig({
-        // Package schema - https://npmjs.org/doc/json.html
         pkg: grunt.file.readJSON('package.json'),
         buildFile: 'build/<%= pkg.name.toLowerCase() %>',
+        distFile: 'dist/toolkit',
 
         // 1) Validate the Javascript source directory
         // http://jshint.com/docs/
@@ -187,18 +123,22 @@ module.exports = function(grunt) {
         // https://github.com/gruntjs/grunt-contrib-concat
         concat: {
             options: {
-                stripBanners: true,
+                banner: banner,
                 separator: ''
             },
             build: {
                 files: [
                     { src: cssPaths, dest: '<%= buildFile %>.min.css' },
-                    { src: jqConcatPaths, dest: '<%= buildFile %>-jquery.min.js' },
-                    { src: mooConcatPaths, dest: '<%= buildFile %>-mootools.min.js' }
+                    { src: jqueryPaths, dest: '<%= buildFile %>-jquery.min.js' },
+                    { src: mooPaths, dest: '<%= buildFile %>-mootools.min.js' }
                 ]
             },
             dist: {
-                files: prepareDistribution(cssPaths, jqConcatPaths, mooConcatPaths)
+                files: [
+                    { src: cssPaths, dest: '<%= distFile %>.min.css' },
+                    { src: jqueryPaths, dest: '<%= distFile %>-jquery.min.js' },
+                    { src: mooPaths, dest: '<%= distFile %>-mootools.min.js' }
+                ]
             }
         },
 
@@ -207,6 +147,8 @@ module.exports = function(grunt) {
         uglify: {
             options: {
                 report: 'min',
+                preserveComments: false,
+                banner: banner,
                 enclose: {
                     window: 'window',
                     jQuery: '$'
@@ -216,21 +158,23 @@ module.exports = function(grunt) {
                 options: {
                     mangle: false,
                     compress: false,
-                    beautify: true,
-                    banner: createBanner()
+                    beautify: true
                 },
                 files: {
                     '<%= buildFile %>-jquery.min.js': '<%= buildFile %>-jquery.min.js',
                     '<%= buildFile %>-mootools.min.js': '<%= buildFile %>-mootools.min.js'
                 }
             },
-            dist: {
-                options: {
-                    banner: createBanner(true)
-                },
+            prod: {
                 files: {
                     '<%= buildFile %>-jquery.min.js': '<%= buildFile %>-jquery.min.js',
                     '<%= buildFile %>-mootools.min.js': '<%= buildFile %>-mootools.min.js'
+                }
+            },
+            dist: {
+                files: {
+                    '<%= distFile %>-jquery.min.js': '<%= distFile %>-jquery.min.js',
+                    '<%= distFile %>-mootools.min.js': '<%= distFile %>-mootools.min.js'
                 }
             }
         },
@@ -242,8 +186,7 @@ module.exports = function(grunt) {
                 replacements: [
                     { pattern: '%version%', replacement: '<%= pkg.version %>' },
                     { pattern: '%build%', replacement: Date.now().toString(36) },
-                    { pattern: /(\r?\n\r?\n)/g, replacement: "\n" },
-                    { pattern: /\r?\n$/g, replacement: "" }
+                    { pattern: /(\r|\n)/g, replacement: "" }
                 ]
             },
             build: {
@@ -255,11 +198,9 @@ module.exports = function(grunt) {
             },
             dist: {
                 files: {
-                    'dist/toolkit.min.css': 'dist/toolkit.min.css',
-                    'dist/toolkit-jquery.min.js': 'dist/toolkit-jquery.min.js',
-                    'dist/toolkit-mootools.min.js': 'dist/toolkit-mootools.min.js',
-                    'dist/jquery/toolkit.min.js': 'dist/jquery/toolkit.min.js',
-                    'dist/mootools/toolkit.min.js': 'dist/mootools/toolkit.min.js'
+                    '<%= distFile %>.min.css': '<%= distFile %>.min.css',
+                    '<%= distFile %>-jquery.min.js': '<%= distFile %>-jquery.min.js',
+                    '<%= distFile %>-mootools.min.js': '<%= distFile %>-mootools.min.js'
                 }
             }
         },
@@ -285,7 +226,7 @@ module.exports = function(grunt) {
         watch: {
             scripts: {
                 files: 'js/**/*.js',
-                tasks: ['newer:uglify:build', 'concat:build']
+                tasks: ['newer:concat:build', 'uglify:build']
             },
             styles: {
                 files: 'scss/**/*.scss',
@@ -306,7 +247,7 @@ module.exports = function(grunt) {
 
     // Register tasks
     grunt.registerTask('validate', ['jshint']);
-    grunt.registerTask('distribute', ['jshint', 'compass:dist', 'uglify:dist', 'concat:dist', 'string-replace:dist']);
-    grunt.registerTask('production', ['jshint', 'compass:dist', 'uglify:dist', 'concat:build', 'string-replace:build']);
+    grunt.registerTask('distribute', ['jshint', 'compass:dist', 'concat:dist', 'uglify:dist', 'string-replace:dist']);
+    grunt.registerTask('production', ['jshint', 'compass:dist', 'concat:build', 'uglify:prod', 'string-replace:build']);
     grunt.registerTask('default', ['jshint', 'compass:build', 'concat:build', 'uglify:build', 'string-replace:build']);
 };
--- changelog.md
+++ changelog.md
@@ -7,6 +7,7 @@ Older versions can be found in the documentation changelogs.
 * Added ARIA support to all components with supported documentation
 * Removed is and has state prefixing from the CSS and JS layers
 * Removed `$state-is-prefix` and `$state-has-prefix` Sass variables
+* Removed individual JavaScript distribution files
 * jQuery
     * Removed `$.hyphenate()`
 * Toolkit
@@ -15,7 +16,7 @@ Older versions can be found in the documentation changelogs.
     * Renamed `Toolkit.options.vendor` to `Toolkit.vendor`
     * Renamed `Toolkit.createComponent()` to `Toolkit.create()`
 * Component
-    * Added static `count` property that tracks the number of instances in the page
+    * Added static `count` property to each component that tracks the number of instances in the page
     * Added `uid` property that represents the current instance count
     * Added `cssClass` property that represents the component name in CSS class form
     * Added `id()` method to generate unique CSS class names
