84e70e05cd90a27faf091846bf6aa7cbb3ef8656
Miles Johnson <miles.johnson@hyfn.com>
2014-03-27 12:54:17 -0700

    Cleaned up demos

--- demo/index.php
+++ demo/index.php
@@ -515,6 +515,8 @@ if ($vendor === 'mootools') {
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta name="charset" content="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>Titon - Toolkit - <?php echo $component['title']; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
