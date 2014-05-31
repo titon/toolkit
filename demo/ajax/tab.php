ca34a1b8cb50b187ef7d9aa6be5ba400c3a7dc18
Miles Johnson <miles.johnson@hyfn.com>
2014-05-30 18:11:31 -0700

    Added stalker and modal demos

--- demo/ajax/tab.php
+++
@@ -1,3 +0,0 @@
-<?php sleep(1); ?>
-
-Tab content loaded with AJAX via href attribute.
--- demo/ajax/tooltip.php
+++
@@ -1,5 +0,0 @@
-<?php if (isset($_GET['slow'])) {
-    sleep(1);
-} ?>
-
-This tooltip is loaded from an AJAX call.
