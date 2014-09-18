4b78f013a7adedde35ea782fed91f877be165716
Miles Johnson <miles.johnson@hyfn.com>
2014-09-18 11:27:15 -0700

    Check for phantomjs

--- gulpfile.js
+++ gulpfile.js
@@ -102,6 +102,12 @@ gulp.task('js', function() {
 });
 
 gulp.task('test', function() {
+    var scriptPath = lookup('mocha-phantomjs/lib/mocha-phantomjs.coffee');
+
+    if (!scriptPath) {
+        throw new gutil.PluginError('Toolkit', 'mocha-phantomjs not found');
+    }
+
     return gulp.src('./tests/**/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
