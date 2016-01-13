a4de16110bf64b8ecbc51d04cabc013abf1badb8
Miles Johnson <mileswjohnson@gmail.com>
2016-01-13 14:15:37 -0800

    Moved prop types to the source root.

--- src/Component.js
+++ src/Component.js
@@ -8,7 +8,7 @@ import React, { PropTypes } from 'react';
 import Titon from 'Titon';
 import ClassBuilder from 'ext/utility/ClassBuilder';
 import assign from '../node_modules/lodash/object/assign';
-import cssClassName from 'ext/prop-types/cssClassName';
+import cssClassName from 'prop-types/cssClassName';
 import generateUID from 'ext/utility/generateUID';
 import 'polyfills/Performance.now.js';
 
