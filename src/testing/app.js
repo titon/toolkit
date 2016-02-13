cc372367dde77ce1a439a3a7477a17c9a9b63e29
Miles Johnson <mileswjohnson@gmail.com>
2016-02-13 14:27:16 -0800

    Updated OffCanvas CSS to make use of flexbox.

--- src/testing/app.js
+++ src/testing/app.js
@@ -16,24 +16,26 @@ const log = function(e, ...args) {
 };
 
 ReactDOM.render(
-    <div style={{ position: 'relative' }}>
-        <OffCanvas swipe={true} showOnLoad={false} animation="squish" multiple={true}>
-            <OffCanvas.MainContent>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed imperdiet nisi in lectus euismod cursus. Nulla facilisi. Nullam gravida eget nunc vel volutpat. Ut interdum dapibus lacus sed volutpat. Quisque rhoncus, turpis id faucibus sodales, lorem justo pulvinar nibh, ut interdum sapien mi vitae velit. Nam vestibulum elit luctus ante tempor, ut bibendum mauris molestie. Vestibulum at pellentesque nulla. Pellentesque ex neque, ullamcorper sit amet lacus vel, tincidunt posuere est. Pellentesque mollis quis diam ut iaculis. Etiam scelerisque lacus vitae mi placerat fringilla. Sed eget augue eu sem pellentesque consectetur. Integer in justo risus. Nullam pellentesque magna sit amet metus aliquam volutpat non vitae lectus. Duis dignissim velit et justo pellentesque placerat. Ut vel sodales sapien. Nam sit amet luctus tellus.</p>
+    <OffCanvas swipe={true} showOnLoad={false} animation="squish" multiple={true}>
+        <OffCanvas.MainContent>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed imperdiet nisi in lectus euismod cursus. Nulla facilisi. Nullam gravida eget nunc vel volutpat. Ut interdum dapibus lacus sed volutpat. Quisque rhoncus, turpis id faucibus sodales, lorem justo pulvinar nibh, ut interdum sapien mi vitae velit. Nam vestibulum elit luctus ante tempor, ut bibendum mauris molestie. Vestibulum at pellentesque nulla. Pellentesque ex neque, ullamcorper sit amet lacus vel, tincidunt posuere est. Pellentesque mollis quis diam ut iaculis. Etiam scelerisque lacus vitae mi placerat fringilla. Sed eget augue eu sem pellentesque consectetur. Integer in justo risus. Nullam pellentesque magna sit amet metus aliquam volutpat non vitae lectus. Duis dignissim velit et justo pellentesque placerat. Ut vel sodales sapien. Nam sit amet luctus tellus.</p>
 
-                <p>Fusce ornare tortor sed maximus consequat. Integer pulvinar dui sed enim viverra pulvinar. Vestibulum consequat fringilla porttitor. Morbi efficitur dictum sem, sed tincidunt metus varius in. Donec nisl augue, pretium vel orci non, efficitur laoreet tortor. Phasellus eget nulla a sem commodo suscipit eu vel lorem. Mauris lectus neque, fermentum sit amet mauris at, imperdiet porttitor eros. Pellentesque iaculis diam quis sem feugiat, et consectetur nisl faucibus. Donec pellentesque diam mollis urna dictum, sed auctor ligula imperdiet. Duis tincidunt dolor nec iaculis viverra.</p>
-            </OffCanvas.MainContent>
+            <p>Fusce ornare tortor sed maximus consequat. Integer pulvinar dui sed enim viverra pulvinar. Vestibulum consequat fringilla porttitor. Morbi efficitur dictum sem, sed tincidunt metus varius in. Donec nisl augue, pretium vel orci non, efficitur laoreet tortor. Phasellus eget nulla a sem commodo suscipit eu vel lorem. Mauris lectus neque, fermentum sit amet mauris at, imperdiet porttitor eros. Pellentesque iaculis diam quis sem feugiat, et consectetur nisl faucibus. Donec pellentesque diam mollis urna dictum, sed auctor ligula imperdiet. Duis tincidunt dolor nec iaculis viverra.</p>
+        </OffCanvas.MainContent>
 
-            <OffCanvas.Sidebar side="left">
-                LEFT
-            </OffCanvas.Sidebar>
+        <OffCanvas.Sidebar side="left">
+            LEFT
+        </OffCanvas.Sidebar>
 
-            <OffCanvas.Sidebar side="right">
-                RIGHT
-            </OffCanvas.Sidebar>
-        </OffCanvas>
+        <OffCanvas.Sidebar side="right">
+            RIGHT
+        </OffCanvas.Sidebar>
+    </OffCanvas>,
+    document.getElementById('app')
+);
 
-        {/*
+/*
+ReactDOM.render(
         <Drop>
             <Drop.Toggle>
                 <button type="button" className="button" onClick={log}>OPEN MENU</button>
@@ -127,8 +129,8 @@ ReactDOM.render(
                 <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi fermentum congue lectus, at imperdiet odio dictum sit amet.</p>
             </Accordion.Item>
         </Accordion>
-        */}
     </div>
     ,
     document.getElementById('app')
 );
+*/
