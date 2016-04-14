86e334bd7a26e2fbe1f20dd7b6e5649df18dc590
Miles Johnson <mileswjohnson@gmail.com>
2016-04-14 13:06:15 -0700

    Updated children prop type to validate against HTML tags.

--- src/prop-types/children.js
+++ src/prop-types/children.js
@@ -14,14 +14,35 @@ import invariant from '../utility/invariant';
  * @returns {Function}
  */
 export default function children(...types) {
-    let instances = new WeakSet(types);
+    let components = new WeakSet(),
+        tags = new Map();
+
+    types.forEach(type => {
+        // HTML tags
+        if (typeof type === 'string') {
+            tags.set(type, type);
+
+        // React components
+        } else {
+            components.add(type);
+        }
+    });
 
     return function childrenPropType(props, propName, componentName) {
         try {
             Children.forEach(props[propName], child => {
-                invariant(instances.has(child.type),
-                    '`%s` does not allow children of type `%s`.', componentName,
-                    (typeof child.type === 'function') ? child.type.name : child.type);
+                let passed = false,
+                    type = child.type;
+
+                if (typeof type === 'string') {
+                    passed = tags.has(type);
+                } else {
+                    passed = components.has(type);
+                    type = type.name;
+                }
+
+                invariant(passed, '`%s` does not allow children of type `%s`.',
+                    componentName, type);
             });
         } catch (e) {
             return e;
