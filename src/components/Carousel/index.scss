65795cbc6df06a639a4eccd0d4455bdeea2c5234
Miles Johnson <mileswjohnson@gmail.com>
2017-01-23 23:13:06 -0800

    Updated copyright year.

--- src/components/Carousel/index.scss
+++ src/components/Carousel/index.scss
@@ -1,5 +1,5 @@
 /**
- * @copyright   2010-2016, The Titon Project
+ * @copyright   2010-2017, The Titon Project
  * @license     http://opensource.org/licenses/BSD-3-Clause
  * @link        http://titon.io
  */
